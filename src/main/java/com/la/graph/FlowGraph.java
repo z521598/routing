7621fcc85638fe9a3034bbbb4428d45155e57e55
576506402@qq.com <576506402@qq.com>
2018-06-03 21:19:43 +0800

    v6 +算法 TODO

--- src/main/java/com/la/graph/FlowGraph.java
+++ src/main/java/com/la/graph/FlowGraph.java
@@ -11,7 +11,6 @@ import java.util.*;
  */
 public class FlowGraph {
 
-
     private static final int MAX = Integer.MAX_VALUE;
     // 表示点与点之间的关系和点的容量
     private int[][] capacityMatrix;
@@ -206,32 +205,55 @@ public class FlowGraph {
 
     // TODO: 2018/6/3
     // 最大容量优先遍历，填充增广路，返回最大流量
-    private int maxFlowByFlowFirst(int[] flow, boolean[] visited, int current) {
-
-        if (current == t) {
-            return flow[t];
+    private List<PathBean> maxFlowByFlowFirst(int s, int t) {
+        List<PathBean> pathBeanList = new ArrayList<>();
+        this.s = s;
+        this.t = t;
+        int maxFlow = 0;
+        // 获取流量
+        int incrementFlow;
+        // 增广路径
+        path = new int[number];
+        for (int i = 0; i < path.length; i++) {
+            path[i] = -1;
         }
-        // 若当前节点为汇点，则说明找到增广路径
-        for (int i = 0; i < capacityMatrix.length; i++) {
-            int capacity = capacityMatrix[current][i];
-            // i != s    ===>   显然不能返回源点
-            // capacity > 0     ===>  容量大于0，即为可达
-            // visited[i] == false      ===>   该节点为被访问到 [这么写更清楚]
-            if (i != s && capacity > 0 && visited[i] == false) {
-                System.out.print(i + " ");
-                // 记录前驱节点，即记录路径
-                path[i] = current;
-                // 各边容量的最小值即为此路径的最大流量
-                flow[i] = Math.min(capacity, flow[current]);
-                visited[i] = true;
-                if (dfs(flow, visited, i) != -1) {
-                    return flow[t];
+        int[] flow = new int[number];
+        // 记录节点是否访问过
+        boolean[] visited = new boolean[number];
+
+        flow[s] = MAX;
+        // 记录节点是否访问过
+        while ((incrementFlow = dijstraGetMaxFlow(flow, visited, s)) != -1) {
+            maxFlow += incrementFlow;
+            // 处理容量残余图
+            int k = t;          // 利用前驱寻找路径
+
+            List<Integer> path = new LinkedList<>();
+            while (k != s) {
+                // 超级汇点不加入路线图
+                if (s != MainFrame.defaultMatrix.length) {
+                    path.add(k);
                 }
+                int last = this.path[k];
+                capacityMatrix[last][k] -= incrementFlow;  // 改变正向边的容量
+                capacityMatrix[k][last] += incrementFlow;  // 改变反向边的容量
+                k = last;
             }
+            path.add(s);
+            pathBeanList.add(new PathBean(path, incrementFlow));
+            flow = new int[number];
+            flow[s] = MAX;
+            visited = new boolean[number];
+            System.out.println();
         }
 
-        // 若汇点未访问到则说明无增广路
-        return -1;
+        return pathBeanList;
+    }
+
+    // TODO: 2018/6/3
+    private int dijstraGetMaxFlow(int[] flow, boolean[] visited, int s) {
+
+        return 0;
     }
 
     public int[][] getCapacityMatrix() {
